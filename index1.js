866ab6d441448f8ad6a10bb34a8dd7e44e2ec495
Zaviyar Butt <zaviyar55@gmail.com>
2026-03-19 18:04:22 +0500

    first commit

---
+++ index1.js
@@ -0,0 +1,23 @@
+import express from "express";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import bodyParser from "body-parser";
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+app.post("/submit", (req, res) => {
+  // req.body only works if you use middleware to parse incoming data.
+  console.log(req.body)
+})
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
