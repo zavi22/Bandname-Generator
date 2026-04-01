09374abf8b175a9ca2c2dcc05b7dc1a65c3cf20f
Zaviyar Butt <zaviyar55@gmail.com>
2026-04-01 11:03:33 +0500

    Updated index4.js with new changes

--- index4.js
+++ index4.js
@@ -1,32 +1,32 @@
-import express from "express";
-import bodyParser from "body-parser";
-import { dirname } from "path";
-import { fileURLToPath } from "url";
+import express from 'express';
+import bodyParser from 'body-parser';
+import {dirname} from 'path';
+import {fileURLToPath} from 'url';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
-
 const app = express();
 const port = 3000;
-var bandName = "";
+let bandName = "";
+// instead of const we used let here bcz we are going to change the value of bandName variable in the middleware function.
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 
-function bandNameGenerator(req, res, next) {
-  console.log(req.body);
-  bandName = req.body["street"] + req.body["pet"];
+function bandNameGenerator(req, res, next){
+  console.log(req.body)
+  bandName = req.body["street"] + " " + req.body["pet"];
   next();
 }
 
 app.use(bandNameGenerator);
 
-app.get("/", (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
 });
 
-app.post("/submit", (req, res) => {
-  res.send(`<h1>Your band name is:</h1><h2>${bandName}✌️</h2>`);
-});
+app.post('/submit', (req, res) => {
+  res.send(`<h1>Your band name is:</h1><h2>${bandName}</h2>`)
+})
 
 app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
-});
+  console.log(`App listening on port ${port}`);
+})
